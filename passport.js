158233c66be6b9f11bdd7756820abe9734352cc9
tatatitutatuay <thirathawat996@gmail.com>
2024-03-04 04:50:29 +0700

    use secret code in passport from config

--- passport.js
+++ passport.js
@@ -1,10 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config/config.env' });
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('./models/User');
+
 
 passport.use(new GoogleStrategy({
-    clientID: "285505551320-p7i19arfrb9qcac9vmdmj38hg1ro9qh6.apps.googleusercontent.com",
-    clientSecret: "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",
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "http://localhost:5000/api/v1/auth/google/callback",
     scope: ["profile", "email"],
   },
